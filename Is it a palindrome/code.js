492bab4ae742512e9630a0ca9cc4a52cef083266
Vitali Chabatar <thechvi@gmail.com>
2022-11-13 13:57:31 +0100

    decided!

--- Is it a palindrome/code.js
+++ Is it a palindrome/code.js
@@ -2,19 +2,19 @@ function isPalindrome(x) {
 	let newStr1 = x.toUpperCase();
 	let newStr2 = newStr1.split('').reverse().join('');
 	if (newStr1 == newStr2) {
-	 // return true
-	 console.log(true)
-	 }
-	 else {
-  //return false
-  console.log(false)}
-  }
-  isPalindrome('a');
-  isPalindrome('aba');
-  isPalindrome('Abba');
-  isPalindrome('hello');
-  isPalindrome('Bob');
-  isPalindrome('Madam');
-  isPalindrome('AbBa');
-  isPalindrome('');
-  
+		// return true
+		console.log(true)
+	}
+	else {
+		//return false
+		console.log(false)
+	}
+}
+isPalindrome('a');
+isPalindrome('aba');
+isPalindrome('Abba');
+isPalindrome('hello');
+isPalindrome('Bob');
+isPalindrome('Madam');
+isPalindrome('AbBa');
+isPalindrome('');
