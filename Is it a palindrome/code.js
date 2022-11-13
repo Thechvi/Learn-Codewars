8a5dd99939541d31ae660073db9118cff50bed14
Vitali Chabatar <thechvi@gmail.com>
2022-11-13 13:56:58 +0100

    decided!

---
+++ Is it a palindrome/code.js
@@ -0,0 +1,20 @@
+function isPalindrome(x) {
+	let newStr1 = x.toUpperCase();
+	let newStr2 = newStr1.split('').reverse().join('');
+	if (newStr1 == newStr2) {
+	 // return true
+	 console.log(true)
+	 }
+	 else {
+  //return false
+  console.log(false)}
+  }
+  isPalindrome('a');
+  isPalindrome('aba');
+  isPalindrome('Abba');
+  isPalindrome('hello');
+  isPalindrome('Bob');
+  isPalindrome('Madam');
+  isPalindrome('AbBa');
+  isPalindrome('');
+  
