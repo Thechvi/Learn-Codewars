ebe12ddc231cb97c04e75ae84070c19f1c50e990
Vitali Chabatar <thechvi@gmail.com>
2022-11-16 12:05:46 +0100

    !

---
+++ CSV representation of array/code.js
@@ -0,0 +1,13 @@
+const array = [
+	[0, 1, 2, 3, 45],
+	[10, 11, 12, 13, 14],
+	[20, 21, 22, 23, 24],
+	[30, 31, 32, 33, 34]]
+
+function toCsvText(array) {
+
+	console.log(array.join('\n'))
+	//return array.join('\n')
+}
+
+toCsvText(array);
