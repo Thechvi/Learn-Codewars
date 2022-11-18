4e3f9fb9bc602be44f4d2e331c2cb04087ac90cb
Vitali Chabatar <thechvi@gmail.com>
2022-11-18 12:35:04 +0100

    done!

--- Easy wallpaper/code.js
+++ Easy wallpaper/code.js
@@ -1,13 +1,15 @@
 function wallpaper(l, w, h) {
+	const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"]
 	if (l > 0 && w > 0 && h > 0) {
-		const numberOfRolls = Math.round(1.15 * (2 * (l + w) / 0.52) / (10 / h))
+		const numberOfRolls = Math.ceil(1.15 * (2 * (l + w) / 0.52) / (10 / h))
 		//console.log(numberOfRolls)
-		const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"]
-		return numbers[numberOfRolls]
+
+		return (numbers[numberOfRolls])
 	}
 
 	else {
-		return "zero"
+		return numbers[0]
 	}
 }
+
 
