98bcd0009c62eef73681ac42fc9c09a272de4e4e
Vitali Chabatar <thechvi@gmail.com>
2022-11-19 01:29:40 +0100

    step1

---
+++ Categorize New Member/code.js
@@ -0,0 +1,18 @@
+
+function openOrSenior(data) {
+	let result = [];
+	for (i = 0; i < data.length; i++) {
+		for (j = 0; j < data[i].length; j++) {
+			if (data[i][0] > 55 && data[i][1] > 7) {
+				console.log(data[i][0], data[i][1])
+				return data[i] = "Senior"
+			}
+			else {
+				return data[i] = "Open"
+			}
+		}
+	}
+
+	return result.push(data[i])
+}
+console.log(openOrSenior([45, 12], [55, 21], [19, -2], [104, 20])) 
