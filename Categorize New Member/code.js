b5adb48580767d544d88b005e208e1547a08e637
Vitali Chabatar <thechvi@gmail.com>
2022-11-24 10:49:01 +0100

    solution1

--- Categorize New Member/code.js
+++ Categorize New Member/code.js
@@ -1,9 +1,15 @@
 
-function openOrSenior(data){
-	function determineMembership(member){
-	  console.log(member[0] >= 55 && member[1] > 7) ? 'Senior' : 'Open';
-	}
-	console.log(data.map(determineMembership));
-  }
+// function openOrSenior(data){
+// 	function determineMembership(member){
+// 	  console.log(member[0] >= 55 && member[1] > 7) ? 'Senior' : 'Open';
+// 	}
+// 	console.log(data.map(determineMembership));
+//   }
 
-  openOrSenior([45, 12],[55,21],[19, -2],[104, 20])
+//   openOrSenior([45, 12],[55,21],[19, -2],[104, 20])
+
+const promise = new Promise(res => {
+	setTimeout(() =>res((2), 0));
+	res(1);
+});
+promise.then(res=> console.log(res))
